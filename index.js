eb2548b6eeed9cf459568b7bc45f2ba38b6d750d
jacksonianrtaylor <50229817+jacksonianrtaylor@users.noreply.github.com>
2022-08-22 13:38:37 -0700

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const dns = require('dns')
+const dns = require('dns');
 
+const urlMod = require('url');
 //added modules
-//const mySecret = process.env['MONGO_URI']
+const mySecret = process.env['MONGO_URI']
 mongoose = require('mongoose')
 bodyParser = require("body-parser");
-const mySecret = process.env['MONGO_URI']
 mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
 const shortId = require('shortid')
 
@@ -89,6 +89,22 @@ const isValidUrl = function(urlString) {
       }
   }
 
+const iValidUrl2 = function(urlString) {
+let regex1 = new RegExp("^https:\/\/.+");
+
+
+  // if(!regex1.test(urlString))
+  // {
+  //   return false
+  // }
+  // if(!regex2.test(urlString))
+  // {
+  //   return false
+  // }
+  
+  return regex1.test(urlString);
+}
+
 
 const options = {
     all:true,
@@ -102,38 +118,45 @@ const options = {
 //also the test case that redirects is potentially failing because it may be trying to redirect to an invalid url
 
 app.route("/api/shorturl").post(function(req, res){
-  url.find({original_url: req.body.url}, 
-    function (err, results) {
-      if (err){return console.error(err)}
-      if(!results.length) 
+  url.find({original_url: req.body["url"]}, 
+    function (err1, results1) {
+      console.log(req.body["url"])
+      if (err1){return console.error(err1)}
+      if(!results1.length) 
       {
-        if (isValidUrl(req.body.url))
+        if (iValidUrl2(req.body["url"]))
         {
-          dns.lookup(req.body.url, options, function(err, adress, family)
+          dns.lookup(urlMod.parse(req.body["url"]).hostname, function(err2, address, family)
           {
-            if (err){   
-              res.json({ "error": "Invalid URL" })
+            if (err2){   
+              console.log("url does not exist")
+              res.json({ "error": "invalid url" })
             }else{
-              let urlObj = new url({original_url: req.body.url})
-              delete urlObj["_id"]
-              delete urlObj["__v"]
-              urlObj.save(function(err, results){
-                if (err) return console.error(err);
-                done(null, results)})
+              console.log("new url")
+              let urlObj = new url({original_url: req.body["url"]})
+              console.log(urlObj)
+              urlObj.save()
               
-              res.json(urlObj)   
+              //potential callback:
+              //function(err3, results2){
+              //if (err3) return console.error(err3);                
+              //done(null, results2)
+              
+              res.json({"original_url": urlObj["original_url"],
+              "short_url": urlObj["short_url"]})
             }
           })           
         }
         else
         {
-          res.json({ "error": "Invalid URL" })
+          console.log("incorrect format")
+          res.json({ "error": "invalid url" })
         }
       }else{
-        urlObj = results[0].toJSON()
-        delete urlObj["_id"]
-        delete urlObj["__v"]
-        res.json(urlObj)
+        console.log("find url")
+        urlObj = results1[0].toJSON()
+        res.json({"original_url": urlObj["original_url"],
+        "short_url": urlObj["short_url"]})
       }})
 })
 
